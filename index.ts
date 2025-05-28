8e22265fdd339ab7a2a680ec9db825d6bb527d53
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2025-05-28 22:46:00 +0530

    final updated dbUrl updated

--- index.ts
+++ index.ts
@@ -153,6 +153,6 @@ app.post('/identify', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
 });
